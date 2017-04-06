875766150b314f0845a7753b026a157fbb2caae0
Rizky Benggolo <travarilo@gmail.com>
2017-04-06 23:16:53 +0700

    add build script and update .gitignore
    Change-Id: I1c6d4f52ee6dc36f38e5165661c334a235f6590c

---
+++ build.sh
@@ -0,0 +1,12 @@
+DEFCONFIG=lineageos_cancro_defconfig
+make ARCH=arm CROSS_COMPILE=~/gcc/bin/arm-linux-androideabi- $DEFCONFIG
+make ARCH=arm CROSS_COMPILE=~/gcc/bin/arm-linux-androideabi-
+rm ~/redkancut/modules/*
+for i in `find -name *.ko`; do cp $i ~/redkancut/modules/; done
+scripts/dtbToolCM -s 2048 -d "qcom,msm-id = <" -2 -o arch/arm/boot/dt.img -p /usr/bin/ arch/arm/boot/
+cp arch/arm/boot/zImage ~/redkancut/
+cp arch/arm/boot/dt.img ~/redkancut/
+cd ~/redkancut
+rm *.zip
+zip -r9 redkancut_cancro.zip * -x README redkancut_cancro.zip
+cp *.zip /media/sf_ubuntu/release/
